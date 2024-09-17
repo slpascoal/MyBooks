fa04f68975d7334796ceda21936918bc1165db0e
Silas L. Pascoal <slpascoal01@gmail.com>
2024-09-17 17:33:37 -0300

    Criação das Requisições AXIOS

--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -1,7 +1,7 @@
 import { Input } from '../Input'
 import styled from 'styled-components'
-import { useState } from 'react'
-import { livros } from './dadosPesquisa'
+import { useEffect, useState } from 'react'
+import { getLivros } from '../../services/livros'
 
 const PesquisaContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -54,6 +54,17 @@ const Resultado = styled.div`
 
 export function Pesquisa() {
   const [livrosPesquisados, setLivrosPesquisados] = useState([])
+  const [livros, setLivros] = useState([])
+
+  useEffect(() => {
+    fetchLivros()
+  }, [])
+
+  async function fetchLivros() {
+    const livrosAPI = await getLivros()
+    setLivros(livrosAPI)
+  }
+
   return (
     <PesquisaContainer>
       <Titulo>Já sabe por onde começar?</Titulo>
